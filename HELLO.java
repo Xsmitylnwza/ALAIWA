32119e67cb2430c834c21872c42e652ceb4a27ec
Xsmitylnwza <111405516+Xsmitylnwza@users.noreply.github.com>
2023-05-22 10:43:12 +0700

    Update HELLO.java

--- HELLO.java
+++ HELLO.java
@@ -1,7 +1,10 @@
 public class HELLO {
     public static void main(String[] args) {
         System.out.println("HELLO WORLD");
+<<<<<<< HEAD
         System.out.println("HELLO YOU TOO");
         System.out.println("KUY");
+=======
+>>>>>>> parent of b9b55b1 (update hello you too)
     }
 }
