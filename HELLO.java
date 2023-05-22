7f396e9142779e9d4c075a101680787bb7751b09
Xsmitylnwza <111405516+Xsmitylnwza@users.noreply.github.com>
2023-05-22 10:42:54 +0700

    s
    s

--- HELLO.java
+++ HELLO.java
@@ -2,5 +2,6 @@ public class HELLO {
     public static void main(String[] args) {
         System.out.println("HELLO WORLD");
         System.out.println("HELLO YOU TOO");
+        System.out.println("KUY");
     }
 }
