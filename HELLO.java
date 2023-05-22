8d080202f9125b8492ffc69dc6aefa7b5b04749b
Xsmitylnwza <111405516+Xsmitylnwza@users.noreply.github.com>
2023-05-22 10:56:12 +0700

    asd
    asd

--- HELLO.java
+++ HELLO.java
@@ -1,10 +1,8 @@
 public class HELLO {
     public static void main(String[] args) {
         System.out.println("HELLO WORLD");
-<<<<<<< HEAD
         System.out.println("HELLO YOU TOO");
         System.out.println("KUY");
-=======
->>>>>>> parent of b9b55b1 (update hello you too)
+        
     }
 }
