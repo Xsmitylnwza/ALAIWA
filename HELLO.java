6eaa2d189bea4d21e1e568b1a1ee07ed4eaaaa00
Xsmitylnwza <111405516+Xsmitylnwza@users.noreply.github.com>
2023-05-22 10:56:26 +0700

    Update HELLO.java

--- HELLO.java
+++ HELLO.java
@@ -3,6 +3,6 @@ public class HELLO {
         System.out.println("HELLO WORLD");
         System.out.println("HELLO YOU TOO");
         System.out.println("KUY");
-        
+        System.out.println("IHERE TOO");
     }
 }
