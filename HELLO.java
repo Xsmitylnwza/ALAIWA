4ecba01d0882b97e0b55e187c7b8c8f7a6b20553
Xsmitylnwza <111405516+Xsmitylnwza@users.noreply.github.com>
2023-05-22 10:45:01 +0700

    Revert "Update HELLO.java"
    This reverts commit 38b91cf4fa2829836fd9aeac89206290843a3a6b.

--- HELLO.java
+++ HELLO.java
@@ -1,8 +1,10 @@
 public class HELLO {
     public static void main(String[] args) {
         System.out.println("HELLO WORLD");
+<<<<<<< HEAD
         System.out.println("HELLO YOU TOO");
         System.out.println("KUY");
-        System.out.println("IHERE TOO");
+=======
+>>>>>>> parent of b9b55b1 (update hello you too)
     }
 }
